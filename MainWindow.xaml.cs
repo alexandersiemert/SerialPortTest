36da22f9256f5306f8ea5e96ee3aad1e234274df
alexandersiemert <alexander@siemert.de>
2023-12-14 11:02:03 +0100

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Threading;
+using System.Diagnostics;
+
+namespace SerialPortTest
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SerialPortManager serialPortManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            serialPortManager = new SerialPortManager("COM3"); // Ersetzen Sie "COMx" mit Ihrer COM-Port-Nummer
+            serialPortManager.DataReceived += OnDataReceived;
+            serialPortManager.OpenPort();
+            _ = serialPortManager.StartReadingAsync();
+        }
+
+        private void btnSendCommand_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                serialPortManager.SendCommand("I");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnDataReceived(string data)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                txtResponse.Text = data;
+            });
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            serialPortManager.StopReading();
+            serialPortManager.ClosePort();
+        }
+    }
+
+
+
+}
---
+++ SerialPortManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace SerialPortTest
+{
+    public class SerialPortManager
+    {
+        private SerialPort serialPort;
+        private StringBuilder dataBuffer = new StringBuilder();
+        public event Action<string> DataReceived;
+        private CancellationTokenSource cancellationTokenSource;
+
+        public SerialPortManager(string portName)
+        {
+            serialPort = new SerialPort(portName, 38400, Parity.None, 8, StopBits.One);
+            cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public void OpenPort()
+        {
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Fehler beim Öffnen des Ports: " + ex.Message);
+            }
+        }
+
+        public void ClosePort()
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        public void SendCommand(string command)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Write(command);
+            }
+            else
+            {
+                throw new InvalidOperationException("Port ist nicht geöffnet.");
+            }
+        }
+
+        public async Task StartReadingAsync()
+        {
+            while (serialPort.IsOpen)
+            {
+                while (!cancellationTokenSource.Token.IsCancellationRequested)
+                {
+                    try
+                    {
+                        await Task.Delay(50);
+                        string data = serialPort.ReadExisting();
+                        dataBuffer.Append(data);
+                        Debug.WriteLine(data);
+
+                        if (dataBuffer.ToString().Contains("\r\n"))
+                        {
+                            string completeMessage = dataBuffer.ToString();
+                            dataBuffer.Clear();
+                            DataReceived?.Invoke(completeMessage);
+                        }
+                    }
+                    catch (TimeoutException)
+                    {
+                        // Timeout-Handling oder ignoriert
+                    }
+                }
+                
+            }
+        }
+
+        public void StopReading()
+        {
+            cancellationTokenSource.Cancel();
+        }
+
+        public void RestartReading()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            _ = StartReadingAsync();
+        }
+    }
+}
