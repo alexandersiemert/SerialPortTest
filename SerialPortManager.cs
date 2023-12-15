b3e9562054921eaa62144f14c59f49d78e92a85f
alexandersiemert <alexander@siemert.de>
2023-12-15 09:00:12 +0100

    #3 Fehler abfangen und Timeout

--- SerialPortManager.cs
+++ SerialPortManager.cs
@@ -8,43 +8,60 @@ using System.Diagnostics;
 using System.Threading;
 using System.Windows.Threading;
 using System.Windows;
+using System.Timers;
 
 namespace SerialPortTest
 {
     public class SerialPortManager
     {
+        //Neuen SerialPort anlegen
         private SerialPort serialPort;
-        private string receivedData = "";
-        public event Action<string> DataReceived;
 
+        //String für das Abspeichern des Empfangspuffers
+        private string receivedData = "";
+
+        //Timer für Timeout bei Datenempfang
+        private System.Timers.Timer dataReceiveTimer;
+
+        //Verlinkung zum DataReceived Event der Instanz des SerialPorts im Main Code mit Übergabe der complete Message
+        public event Action<string> DataReceived;
+ 
+        //Initialisiere SerialPort
         public SerialPortManager(string portName)
         {
             serialPort = new SerialPort(portName, 38400, Parity.None, 8, StopBits.One);
             serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+            //Timer für Timeout anlegen
+            dataReceiveTimer = new System.Timers.Timer(5000); // 5000 Millisekunden (5 Sekunden)
+            dataReceiveTimer.Elapsed += DataReceiveTimer_Elapsed;
         }
 
+        //Get COM-Port Name
         public string GetPortName()
         {
             return serialPort.PortName;
         }
 
+        //SerialPort öffnen
         public void OpenPort()
         {
             try
             {
                 serialPort.Open();
             }
-            catch (Exception ex)
+            catch
             {
                 MessageBox.Show("An error has occurred: " + serialPort.PortName + " not found", "COM-Port Error", MessageBoxButton.OKCancel, MessageBoxImage.Error);
             }
         }
 
+        //Abfrage ob Port offen
         public bool IsOpen()
         {
             return serialPort.IsOpen;
         }
 
+        //COM-Port schließen
         public void ClosePort()
         {
             if (serialPort.IsOpen)
@@ -53,18 +70,21 @@ namespace SerialPortTest
             }
         }
 
+        //Befehl senden
         public void SendCommand(string command)
         {
             try
             {
                 serialPort.Write(command);
+                StartDataReception();
             }
-            catch(Exception ex)
+            catch
             {
                 MessageBox.Show("An error has occurred: " + serialPort.PortName + " not open", "COM-Port Error", MessageBoxButton.OKCancel, MessageBoxImage.Error);
             }        
         }
 
+        //Event wenn Daten empfangen wurden
         private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             try
@@ -72,22 +92,45 @@ namespace SerialPortTest
                 // Daten vom COM-Port lesen und an den empfangenen Datenstring anhängen
                 receivedData += serialPort.ReadExisting();
                 Debug.WriteLine(receivedData);
-
+                // Timeout Stoppen
+                StopDataReception();
+                // Timeout wieder starten für nächsten Datenblock
+                StartDataReception();
                 // Überprüfen, ob die Daten mit CR LF enden
                 if (receivedData.EndsWith("\r\n"))
                 {
+                    // Stoppe Timeout Timer bei komplettem Datenempfang
+                    StopDataReception();
                     // Speichere das Datenpaket ohne CR LF am Ende ab
                     string completeMessage = receivedData.Substring(0, receivedData.Length - "\r\n".Length);
                     // Zurücksetzen des empfangenen Datenstrings
                     receivedData = "";
-                    // Sende das komplette Datenpaket an das Received Event der jeweiligen Instanz des SerialPorts
+                    // Verlinke das komplette Datenpaket an das Data Received Event der jeweiligen Instanz des SerialPorts
                     DataReceived?.Invoke(completeMessage);
                 }
             }
             catch(Exception ex)
             {
-                MessageBox.Show("An error has occurred: " + serialPort.PortName + ": " + ex.Message, "COM -Port Error", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+                MessageBox.Show("An error has occurred: " + serialPort.PortName + ": " + ex.Message, "COM-Port Error", MessageBoxButton.OKCancel, MessageBoxImage.Error);
             }
+        }
+        // Starten Sie den Timer, wenn Sie auf Daten warten
+        private void StartDataReception()
+        {
+            dataReceiveTimer.Start(); //Info: Der Timer fängt immer wieder bei der usprünglich festgelegten Zeit an
+        }
+
+        // Stoppen Sie den Timer, wenn Daten empfangen wurden oder ein Timeout erreicht wurde
+        private void StopDataReception()
+        {
+            dataReceiveTimer.Stop();
+        }
+
+        private void DataReceiveTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            MessageBox.Show("Timeout: " + serialPort.PortName + " not responding.", "COM-Port Error", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+            // Timeout Timer anhalten
+            StopDataReception();
         }
 
     }
